5caa4c090da89d21a5a82f758b0934c124f7be2c
Sanja <sanja1mandic@gmail.com>
2022-06-09 11:39:37 +0200

    made interations

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -8,24 +8,21 @@ const taskData = new Task('task1',false,1);
 
 
 describe('functions responsible for adding and deleting list items', () => {
+  test('Make new object from class Task', () => {
+    expect(taskData).toEqual({description: 'task1', completed: false, index: 1})
+  });
 
-test('Make new object from class Task', () => {
+  test('Add task', () => {
+    addTask('task1',false,1);
+    const taskUI = document.querySelectorAll('.task');
+    expect(taskUI).toHaveLength(1);
+    expect(list).toEqual([{description: 'task1', completed: false, index: 1}])
+  });
 
-  expect(taskData).toEqual({description: 'task1', completed: false, index: 1})
-
-});
-
-test('Add task', () => {
-  addTask('task1',false,1);
-  const taskUI = document.querySelectorAll('.task');
-  expect(taskUI).toHaveLength(1);
-  expect(list).toEqual([{description: 'task1', completed: false, index: 1}])
-});
-
-test('Remove task', () => {
-  clearTask(1, list);
-  const taskUI = document.querySelectorAll('.task');
-  expect(taskUI).toHaveLength(0);
-  expect(list).toEqual([])
-});
+  test('Remove task', () => {
+    clearTask(1, list);
+    const taskUI = document.querySelectorAll('.task');
+    expect(taskUI).toHaveLength(0);
+    expect(list).toEqual([])
+  });
 })
